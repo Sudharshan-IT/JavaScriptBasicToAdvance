692f3e967d7b6dbf6906bb5d48d22ba94788978c
Sudharshan <sudharshanjavaguide@gmail.com>
2023-12-05 11:51:01 +0530

    Added Number Variable

--- Variables.js
+++ Variables.js
@@ -1,4 +1,5 @@
 var name="Nandyala Sudharshan";
-console.log(`Welcome To My Home MR.${name} Yadav`);
+var number=123
+console.log(`Welcome To My Home MR.${name} Yadav His Number is ${number}`);
 var condtion=(2<1 ? ture : false);
 console.log(condtion);
